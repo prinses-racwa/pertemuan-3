d5200fb9867de931e48bde24e1f1fc8ce045151d
prinses-racwa <rajwaamelia@gmail.com>
2026-03-05 11:06:01 +0700

    add procedur

--- 115_bubbleSort.cpp
+++ 115_bubbleSort.cpp
@@ -4,3 +4,16 @@ using namespace std;
 
 int arr[20]; // Deklarasi variabe global array a dengan ukuran 20
 int n; // Deklarasi variabel global n untuk menyimpan banyaknya elemen
+
+void input() { // procedur untuk input
+    while (true) {
+        cout << "Masukkan banyaknya elemen pada array :"
+        cin >> n;
+        if (n <= 20)
+            break;
+        else {
+            cout << "\nArray dapat mempunyai maksimal 20 elemen. \n";
+        }
+    }
+
+}
