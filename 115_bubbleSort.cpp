8d2392c85ae16574cebb5fafe333e1d124b67134
prinses-racwa <rajwaamelia@gmail.com>
2026-03-05 10:48:36 +0700

    add deklarasi

--- 115_bubbleSort.cpp
+++ 115_bubbleSort.cpp
@@ -1,3 +1,6 @@
 #include <ionstream>
 #include <string>
 using namespace std;
+
+int arr[20]; // Deklarasi variabe global array a dengan ukuran 20
+int n; // Deklarasi variabel global n untuk menyimpan banyaknya elemen
