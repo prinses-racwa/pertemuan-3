796c5b214b638b3011e9f7688047906c97118dab
prinses-racwa <rajwaamelia@gmail.com>
2026-03-05 11:14:40 +0700

    add elemen

--- 115_bubbleSort.cpp
+++ 115_bubbleSort.cpp
@@ -15,5 +15,13 @@ void input() { // procedur untuk input
             cout << "\nArray dapat mempunyai maksimal 20 elemen. \n";
         }
     }
+    cout << endl;
+    cout << "==========" << endl;
+    cout << "Masukkan Elemen Array" << endl;
+    cout << "==========" << endl;
 
+    for (int i = 0; i < n; i++) {
+        cout << "Data ke-" << (i + 1) << ": ";
+        cin >> arr[i];
+    }
 }
